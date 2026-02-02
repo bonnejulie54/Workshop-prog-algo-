468473480ec8127cb6ca437a0e86e06412fdd223
Julie Bonne <juliebonne@MacBook-Pro-de-Julie.local>
2026-02-02 10:58:47 +0100

    add 3 exrecices

---
+++ build/CMakeFiles/ImageEditor.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImageEditor.dir/lib/random.cpp.o"
+  "CMakeFiles/ImageEditor.dir/lib/random.cpp.o.d"
+  "CMakeFiles/ImageEditor.dir/src/main.cpp.o"
+  "CMakeFiles/ImageEditor.dir/src/main.cpp.o.d"
+  "ImageEditor"
+  "ImageEditor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImageEditor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/lib/sil/lib/img/CMakeFiles/stb_image.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/juliebonne/Desktop/ECOLE/IMAC/semestre_1/prog algo/workshop/S1-Prog-Workshop/lib/sil/lib/img/lib/stb_image/stb_image.cpp" "lib/sil/lib/img/CMakeFiles/stb_image.dir/lib/stb_image/stb_image.cpp.o" "gcc" "lib/sil/lib/img/CMakeFiles/stb_image.dir/lib/stb_image/stb_image.cpp.o.d"
+  "/Users/juliebonne/Desktop/ECOLE/IMAC/semestre_1/prog algo/workshop/S1-Prog-Workshop/lib/sil/lib/img/lib/stb_image/stb_image_write.cpp" "lib/sil/lib/img/CMakeFiles/stb_image.dir/lib/stb_image/stb_image_write.cpp.o" "gcc" "lib/sil/lib/img/CMakeFiles/stb_image.dir/lib/stb_image/stb_image_write.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
